47031374e86026409364c694634bcfb65a402cf3
Ankit Prajapati <kingprajapati31@gmail.com>
2021-11-18 15:22:45 +0530

    Init commit

---
+++ src/db/mongoose.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose')
+
+
+mongoose.connect(process.env.MONGODB_URL, {
+    // useNewUrlParser: true,
+    // useCreateIndex: true
+    // useFindAndModify: false
+})
+
+
+// const me = new User({
+//     name: '   sparky  ',
+//     email: 'MYEMAIL@MEAD.IO   ',
+//     password: 'phone123!'
+// })
+
+// me.save().then(() => {
+//     console.log(me)
+// }).catch((error) => {
+//     console.log('Error!', error)
+// })
+
+
+
+// const task = new Task({
+//     description: '    Eat lunch'
+// })
+
+// task.save().then(() => {
+//     console.log(task)
+// }).catch((error) => {
+//     console.log(error)
+// })
