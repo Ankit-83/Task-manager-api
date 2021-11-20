cf06401fa64a178926535ebf4c20a2105a408c4d
Ankit Prajapati <kingprajapati31@gmail.com>
2021-11-20 20:03:57 +0530

    Init commit

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ const express = require('express')
 require('./db/mongoose')
 const userRouter = require('./routers/user')
 const taskRouter = require('./routers/task')
+const swaggerUi = require('swagger-ui-express')
+const swaggerDocument = require('./swagger.json')
 
 const app = express()
 const port = process.env.PORT
@@ -9,6 +11,7 @@ const port = process.env.PORT
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
+app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
